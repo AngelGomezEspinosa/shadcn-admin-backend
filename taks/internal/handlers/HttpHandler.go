e227d353d2d12cc56085565ca358b6f6eb17d4d4
Ángel Gómez Espinosa <98283838+AngelGomezEspinosa@users.noreply.github.com>
2025-03-28 23:04:47 +0100

    Apaño para los CORS

--- taks/internal/handlers/HttpHandler.go
+++ taks/internal/handlers/HttpHandler.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/AngelGomezEspinosa/shadcn-admin-backend/internal/core/ports"
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +15,26 @@ func NewHttpHandler(taskService ports.TaskService) *HttpHandler {
 	return &HttpHandler{TaskService: taskService}
 }
 
+func enableCors(w *http.ResponseWriter) {
+	// Permitir cualquier origen
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	// Permitir ciertos métodos
+	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+	// Permitir ciertos encabezados
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Llamar a la función de habilitación de CORS
+	enableCors(&w)
+
+	if r.Method == "OPTIONS" {
+		// Responder a la solicitud OPTIONS (pre-flight request)
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+}
+
 func (h *HttpHandler) FindAll(c *gin.Context) {
 	task, err := h.TaskService.FindAll()
 	if err != nil {
@@ -20,5 +42,6 @@ func (h *HttpHandler) FindAll(c *gin.Context) {
 		return
 	}
 	// Handle task
+	handler(c.Writer, c.Request)
 	c.JSON(200, task)
 }
